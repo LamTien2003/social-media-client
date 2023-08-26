04c810e0d9fac8e4fa69d1e7be048e178cb27cee
LamTien2003 <ltt209514@gmail.com>
2023-08-26 22:40:31 +0700

    fix bug when deploy socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? 'undefined' : 'http://localhost:3000';
+const URL =
+    process.env.NODE_ENV === 'production' ? 'https://lamthanhtien-socialmedia.netlify.app/' : 'http://localhost:3000';
 
 export const socket = io(URL, {
     autoConnect: false,
